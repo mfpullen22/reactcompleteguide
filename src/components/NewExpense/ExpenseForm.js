a6626c5192362a1de1517fde811ec2138a23c07a
Matthew Pullen <matthew.pullen@gmail.com>
2022-02-10 16:30:44 -0600

    Finished Section 4

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -29,7 +29,7 @@ function ExpenseForm(props) {
         props.onSaveExpenseData(expenseData);
 
         setEnteredTitle("");
-        setEnteredAmount("");
+        setEnteredAmount("");  
         setEnteredDate("");
     };
 
